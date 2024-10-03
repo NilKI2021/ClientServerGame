60e048f2cb93301e60da65627ea75949be107297
MarkiianLastovetskyi <markiian.lastovetskyi.ki.2021@lpnu.ua>
2024-10-04 00:01:02 +0300

    push c# code for client server game

---
+++ ClientSide/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace RockPaperScissorsClient
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("127.0.0.1", 5000);
+            Console.WriteLine("Пiдключення до сервера успiшне!");
+            NetworkStream stream = client.GetStream();
+
+            while (true)
+            {
+                Console.WriteLine("Введiть свiй вибiр (камiнь, ножицi, папiр) або 'вихiд' для завершення:");
+                string userChoice = Console.ReadLine();
+                byte[] dataToSend = Encoding.UTF8.GetBytes(userChoice);
+                stream.Write(dataToSend, 0, dataToSend.Length);
+                if (userChoice.ToLower() == "вихiд")
+                {
+                    Console.WriteLine("Ви вийшли з гри.");
+                    break;
+                }
+
+                byte[] buffer = new byte[256];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string result = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Результат: {result}");
+            }
+            client.Close();
+        }
+    }
+}
---
+++ ServerSide/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace RockPaperScissorsServer
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 5000);
+            server.Start();
+            Console.WriteLine("Сервер запущено, очiкування пiдключення клiєнтiв...");
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("Клiєнт пiдключився!");
+            NetworkStream stream = client.GetStream();
+            string[] options = { "камiнь", "ножицi", "папiр" };
+            Random random = new Random();
+
+            while (true)
+            {
+
+                byte[] buffer = new byte[256];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string clientChoice = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Вибiр клiєнта: {clientChoice}");
+
+
+                if (clientChoice.ToLower() == "вихiд")
+                {
+                    Console.WriteLine("Клiєнт вийшов iз гри.");
+                    break;
+                }
+
+                string serverChoice = options[random.Next(options.Length)];
+                Console.WriteLine($"Вибiр сервера: {serverChoice}");
+                string result;
+                if (clientChoice == serverChoice)
+                {
+                    result = "Нiчия!";
+                }
+                else if ((clientChoice == "камiнь" && serverChoice == "ножицi") ||
+                         (clientChoice == "ножицi" && serverChoice == "папiр") ||
+                         (clientChoice == "папiр" && serverChoice == "камiнь"))
+                {
+                    result = "Ви перемогли!";
+                }
+                else
+                {
+                    result = "Ви програли!";
+                }
+
+                string message = $"Ваш вибiр: {clientChoice}, Вибiр сервера: {serverChoice} - {result}";
+                byte[] dataToSend = Encoding.UTF8.GetBytes(message);
+                stream.Write(dataToSend, 0, dataToSend.Length);
+            }
+
+            client.Close();
+            server.Stop();
+            Console.WriteLine("Сервер завершив роботу.");
+        }
+    }
+}
